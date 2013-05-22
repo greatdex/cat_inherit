183e0c24e134f15e35fc971aa2612319d30ee4b7
Deck Trout <andflyaway@naver.com>
2013-05-22 16:00:17 +0900

    cat-slug compatible

--- cat_inherit.php
+++ cat_inherit.php
@@ -1,30 +1,32 @@
 <?php
+/* category inheritance */ 
 add_action('template_redirect', 'inherit_cat_template');
 
 function inherit_cat_template() {
 
-	if (is_category()) {
+    if (is_category()) {
 
-		$catid = get_query_var('cat');
+        $catid = get_query_var('cat');
+        $catname = get_query_var('category_name');
 
-		if ( file_exists(TEMPLATEPATH . '/category-' . $catid . '.php') ) {
-			include( TEMPLATEPATH . '/category-' . $catid . '.php');
-			exit;
-		}
+        if ( file_exists(TEMPLATEPATH . '/category-' . $catname . '.php') ) {
+            include( TEMPLATEPATH . '/category-' . $catname . '.php');
+            exit;
+        }
 
-		$cat = &get_category($catid);
+        $cat = &get_category($catid);
 
-		$parent = $cat->category_parent;
+        $parent = $cat->category_parent;
 
-		while ($parent){
-			$cat = &get_category($parent);
-			if ( file_exists(TEMPLATEPATH . '/category-' . $cat->cat_ID . '.php') ) {
-				include (TEMPLATEPATH . '/category-' . $cat->cat_ID . '.php');
-				exit;
-			}
-			$parent = $cat->category_parent;
-		}
-	}
+        while ($parent){
+            $cat = &get_category($parent);
+            if ( file_exists(TEMPLATEPATH . '/category-' . $cat->slug . '.php') ) {
+                include (TEMPLATEPATH . '/category-' . $cat->slug . '.php');
+                exit;
+            }
+            $parent = $cat->category_parent;
+        }
+    }
 }
 
 ?>
